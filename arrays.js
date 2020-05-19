bf45d838edbd42af0b1150431aa651d21a5da6a5
Jackie Kinsler <jackiekinsler@gmail.com>
2020-05-19 16:12:53 -0700

    Initial commit.

---
+++ arrays.js
@@ -0,0 +1,25 @@
+"use strict";
+
+
+// 1. printIndices
+function printIndices(items) {
+
+for (const idx in items) {
+  for (const item of items) {
+    console.log(`${ item } ${ idx }`)
+  }
+
+}
+}
+
+
+// 2. everyOtherItem
+function everyOtherItem(items) {
+  // Replace this with your code
+}
+
+
+// 3. smallestNItems
+function smallestNItems(items, n) {
+  // Replace this with your code
+}
