7875565ceb6c19ca88b259c377837bc4454aae80
Jackie Kinsler <jackiekinsler@gmail.com>
2020-05-19 17:26:55 -0700

    completed function in objects.js

--- functions.js
+++ functions.js
@@ -1,16 +1,30 @@
 "use strict";
 
+function isHometown(town) {
+  
+    return (town === 'Lake Angelus');
+}
 
-// 1. isHometown
+function getFullName(firstName, lastName) {
+    
+    return (`${ firstName } ${ lastName }`)
+}
 
-// Define your function here
+function calculateTotal(basePrice, state, tax = 0.05) {
 
+    const subtotal = basePrice * (1 + tax);
 
-// 2. getFullName
+    let fee = 0; 
 
-// Define your function here
+    if (state === 'CA') {
+       fee = 0.03 * subtotal; 
+    } else if (state === 'PA') {
+        fee = 2;
+    } else if (state === 'MA') {
+        if (basePrice <= 100) {
+            fee = 1; 
+        } else { fee = 3; }
+    }
 
-
-// 3. calculateTotal
-
-// Define your function here
+    return (subtotal + fee); 
+}
